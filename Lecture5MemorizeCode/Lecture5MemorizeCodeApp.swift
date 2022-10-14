a2c0ab01dd19569b0af0cc9c2bedecb1bf8aca2f
guochen <chenguo0313@gmail.com>
2022-10-14 06:42:34 -0400

    Initial Commit

---
+++ Lecture5MemorizeCode/Lecture5MemorizeCodeApp.swift
@@ -0,0 +1,19 @@
+//
+//  Lecture4MemorizeApp.swift
+//  Lecture4Memorize
+//
+//  Created by Guo Chen on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Lecture5MemorizeApp: App {
+    private let game = EmojiMemoryGame()
+    
+    var body: some Scene {
+        WindowGroup {
+            EmojiMemoryGameView(game: game)
+        }
+    }
+}
